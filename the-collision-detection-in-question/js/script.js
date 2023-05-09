dfd35c695572dad21fe67ac76bc753ab895ca36a
PhillUndSoos <ph_hass@web.de>
2023-05-09 15:18:26 +0200

    Update script.js
    Collision detection for rectangles
    (if/else block)

---
+++ the-collision-detection-in-question/js/script.js
@@ -0,0 +1,20 @@
+const rect1 = { x: 5, y: 5, width: 50, height: 50 };
+const rect2 = { x: 20, y: 10, width: 10, height: 10 };
+
+
+//collision detection between rectangles
+if (
+    rect1.x < rect2.x + rect2.width ||
+    rect1.x + rect1.width > rect2.x ||
+    rect1.y < rect2.y + rect2.height ||
+    rect1.y + rect1.height > rect2.y
+) {
+    //no collision
+    console.log(`No collision detected for ${rect1} and ${rect2}`)
+} else {
+    //collision
+    console.log(`Collision detected for ${rect1} and ${rect2}`)
+}
+
+
+
